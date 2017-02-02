19412ffe0802a64d206602d2874ce2622f52fd09
minarhee <mxr536@miami.edu>
2017-02-02 12:33:33 -0500

    HomeworkWeek2

---
+++ Week 2 Assignment/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas(500,500);
+
+}
+
+function draw() {
+;
+//snowman
+    fill(175,100,220);
+    ellipse(200,215,25,25);
+    ellipse(200,250,50,50);
+    ellipse(200,310,80,80);
+ //arms
+    fill(255,170,238);
+    rect(225,240,50,10);
+    rect(125,240,50,10);
+    
+    //nose
+    fill(255,170,220);
+    triangle(195,215,205,215,200,216)
+}
